dd55647c4f5d102e6813eac905ed362670f8b424
Victoria <vep002@gmail.com>
2021-05-04 18:47:51 -0500

    commit tree e0805d734a6cb2bedbc95c4e17a8184d2c07c6bc

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, name) {
+    return drivers.filter(function (name) {
+        return name
+    })
+}
+
+function fuzzyMatch() {
+
+}
+
+function matchName() {
+
+}
